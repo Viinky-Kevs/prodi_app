04b8ccaa80f5ad5621200085e4b8e3be7760280b
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2022-05-19 13:54:11 -0500

    Commit

--- lib/index.dart
+++ lib/index.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class IndexHome extends StatelessWidget{
+class IndexHome extends StatefulWidget{
   const IndexHome({Key? key}) : super(key: key);
+  @override
+  State<IndexHome> createState() => _IndexHomeState();
+}
 
+class _IndexHomeState extends State<IndexHome>{
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: SingleChildScrollView(
-          child: Row(
-            children: [Text('Pro-diplosis toma de datos',
-                style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
-              textAlign: TextAlign.center,),],
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20,),
+              const Text(
+                  'Toma de datos de prodiplosis',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  color: Colors.lightGreen,
+                ),
+              ),
+              const SizedBox(height: 20,),
+              const Text(
+                'Esta aplicación facilita la toma de datos de Prodiplosis sp. para el modelamiento de la población y el planteamiento de un manejo fitosanitario que permita controlar',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 20,),
+              const Text(
+                  'Podrá visualizar en un mapa interactivo el comportamiento poblacional, además de tener en una sección la información de los resultados del muestreo.',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 20,),
+              Column(
+                children: [
+                  Image.asset('assets/images/image1.jpeg'),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+  
 }
