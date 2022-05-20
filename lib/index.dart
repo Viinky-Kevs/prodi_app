ab2aaae1cab10f849394eb7465c54eba9b4fa0b8
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2022-05-20 09:03:44 -0500

    Commit

--- lib/index.dart
+++ lib/index.dart
@@ -12,13 +12,13 @@ class _IndexHomeState extends State<IndexHome>{
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Center(
           child: Column(
             children: [
               const SizedBox(height: 20,),
               const Text(
-                  'Toma de datos de prodiplosis',
+                  'Toma de datos de Prodiplosis',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 28,
@@ -42,10 +42,114 @@ class _IndexHomeState extends State<IndexHome>{
                 textAlign: TextAlign.justify,
               ),
               const SizedBox(height: 20,),
-              Column(
-                children: [
-                  Image.asset('assets/images/image1.jpeg'),
-                ],
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 20.0),
+                height: 250,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 360.0,
+                      child: Card(
+                        child: Wrap(
+                          children: <Widget>[
+                            Image.asset('assets/images/image1.jpeg'),
+                            const ListTile(
+                              title: Text('Cultivo de Ruscus (Cachipay-Cundinamarca'),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 360.0,
+                      child: Card(
+                        child: Wrap(
+                          children: <Widget>[
+                            Image.asset('assets/images/image2.jpeg'),
+                            const ListTile(
+                              title: Text('Larva de Prodiplosis'),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 360.0,
+                      child: Card(
+                        child: Wrap(
+                          children: <Widget>[
+                            Image.asset('assets/images/image3.jpeg'),
+                            const ListTile(
+                              title: Text('Daños en Ruscus por Prodiplosis'),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20,),
+              const Text(
+                'Equipo de Trabajo',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  color: Colors.lightGreen,
+                ),
+              ),
+              const SizedBox(height: 20,),
+              const Text(
+                'Este proyecto es ejecutado por profesores, estudiantes de posgrado y pregado de la Universidad Nacional de Colombia sede Bogotá DC.',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 20.0),
+                height: 250,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 360.0,
+                      child: Card(
+                        child: Wrap(
+                          children: <Widget>[
+                            Image.asset('assets/images/joaquin.jpg'),
+                            const ListTile(
+                              title: Text('phD Joaquín Ramirez'),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 360.0,
+                      child: Card(
+                        child: Wrap(
+                          children: <Widget>[
+                            Image.asset('assets/images/Guido.jpg'),
+                            const ListTile(
+                              title: Text('phD Guido Plaza'),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20,),
+              const Text(
+                'Contáctanos',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  color: Colors.lightGreen,
+                ),
               ),
             ],
           ),
